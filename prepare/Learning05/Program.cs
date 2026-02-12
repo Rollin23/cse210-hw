f11ced6ddd09ba707cf183ffc3fae0d50d89e29c
Stephen Rollins <rollin23@byui.edu>
2026-02-12 10:28:17 -0700

    testing

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -7,15 +7,15 @@ class Program
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
 
-        Fraction f2 = new Fraction(5);
+        Fraction f2 = new Fraction(2);
         Console.WriteLine(f2.GetFractionString());
         Console.WriteLine(f2.GetDecimalValue());
 
-        Fraction f3 = new Fraction(3, 4);
+        Fraction f3 = new Fraction(1, 9);
         Console.WriteLine(f3.GetFractionString());
         Console.WriteLine(f3.GetDecimalValue());
 
-        Fraction f4 = new Fraction(1, 3);
+        Fraction f4 = new Fraction(8, 5);
         Console.WriteLine(f4.GetFractionString());
         Console.WriteLine(f4.GetDecimalValue());
 
@@ -23,8 +23,8 @@ class Program
         Fraction f5 = new Fraction();
         for (int i = 0; i < 20; i++)
         {
-            int topValue = random.Next(1, 11);
-            int bottomValue = random.Next(1, 11);
+            int topValue = random.Next(3, 10);
+            int bottomValue = random.Next(12, 8);
             f5.SetTop(topValue);
             f5.SetBottom(bottomValue);
             Console.Write($"Fraction {i + 1}: ");
