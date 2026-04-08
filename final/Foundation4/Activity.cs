5614fcdaed7af5b4762589061a7465f6d96a4737
Stephen Rollins <rollin23@byui.edu>
2026-04-07 22:19:15 -0600

    chnges

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,25 @@
+public class Activity
+{
+    private string _date;
+    private int _minute;
+
+    public virtual double GetDistance()
+    {
+        
+    }
+
+    public virtual double Getspeed()
+    {
+        _speed = _distance/
+    }
+
+    public virtual double GetPace()
+    {
+        
+    }
+
+    public string GetSummary()
+    {
+        
+    }
+}
